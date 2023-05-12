2ac7eacf5bcb7eb5cfaa7dab676102944145025b
stephen Batelaan <sbatelaan91@gmail.com>
2023-05-12 08:16:28 -0700

    edited some stuff

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,6 +3,10 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
+    // Store the triggered events
     window.deferredPrompt = event;
 
     // Remove the hidden class from the button.
